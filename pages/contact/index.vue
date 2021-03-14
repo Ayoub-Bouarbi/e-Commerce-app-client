096741fa498d6253ea55cb90c4f4070a6c91b0eb
Ayoub Bouarbi <bouarbi.ayoub1997@gmail.com>
2021-03-14 13:14:55 +0100

    fix contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -11,26 +11,26 @@
         <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
         <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
         <div class="relative mb-4">
-          <label for="email" class="leading-7 text-sm text-gray-600">Name</label>
-          <input type="email" id="email" name="email"
+          <label class="leading-7 text-sm text-gray-600">Name</label>
+          <input type="text" v-model="input.name"
             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
         </div>
         <div class="relative mb-4">
-          <label for="email" class="leading-7 text-sm text-gray-600">Subject</label>
-          <input type="email" id="email" name="email"
+          <label class="leading-7 text-sm text-gray-600">Subject</label>
+          <input type="text" v-model="input.subject"
             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
         </div>
         <div class="relative mb-4">
           <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-          <input type="email" id="email" name="email"
+          <input type="email" id="email" v-model="input.email" name="email"
             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
         </div>
         <div class="relative mb-4">
           <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-          <textarea id="message" name="message"
+          <textarea id="message" name="message" v-model="input.message"
             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
         </div>
-        <button
+        <button @click="contact"
           class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Contact</button>
         <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.
         </p>
